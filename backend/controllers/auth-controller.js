f8b7b027743d2b6537e9ccc749ddbf805e2025cf
vivek napit <viveknapit12@gmail.com>
2023-08-20 19:18:24 +0530

    OTP service working

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,72 +1,80 @@
-const otpService = require('../services/otp-service');
-const hashService = require('../services/hash-service');
-const UserService = require('../services/user-service');
-const userService = require('../services/user-service');
-const tokenService = require('../services/token-service');
-class AuthController{
-    async sendOtp(req , res) {
-        const { phone } = req.body;
+const otpService = require("../services/otp-service");
+const hashService = require("../services/hash-service");
+const UserService = require("../services/user-service");
+const userService = require("../services/user-service");
+const tokenService = require("../services/token-service");
+class AuthController {
+  async sendOtp(req, res) {
+    const { phone } = req.body;
 
-        if (!phone) {
-            res.status(400).json({ message: 'Phone field is required!' });
-        }
-        // res.send(`${phone}`);
-        // generate otp
-        const otp = await otpService.generateOtp();
+    if (!phone) {
+      res.status(400).json({ message: "Phone field is required!" });
+    }
+    // res.send(`${phone}`);
+    // generate otp
+    const otp = await otpService.generateOtp();
 
-        //hash
-        const ttl = 1000 * 60 *2;
-        const expires = Date.now() + ttl;
-        const data = `${phone}.${otp}.${expires}`;
-        const hash = hashService.hashOtp(data);
+    //hash
+    const ttl = 1000 * 60 * 2;
+    const expires = Date.now() + ttl;
+    const data = `${phone}.${otp}.${expires}`;
+    const hash = hashService.hashOtp(data);
 
-        //send otp
-        try{
-            await otpService.sendBySms(phone, otp);
-            return res.json({
-                hash: `${hash}.${expires}`,
-                phone,
-            })
-        }catch(err){
-            console.log(err);
-            res.status(500).json({message: 'message sending failed'});
-        }
+    //send otp
+    try {
+      await otpService.sendBySms(phone, otp);
+      return res.json({
+        hash: `${hash}.${expires}`,
+        phone,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "message sending failed" });
+    }
+  }
+
+  async verifyOtp(req, res) {
+    // logic
+    const { otp, hash, phone } = req.body;
+    if (!otp || !hash || !phone) {
+      res.status(400).json({ message: "All field are required" });
+    }
+    const [hashedOtp, expires] = hash.split(".");
+    if (Date.now() > +expires) {
+      res.status(400).json({ message: "otp has expired" });
+    }
+    const data = `${phone}.${otp}.${expires}`;
+
+    const isValid = otpService.verifyOtp(hashedOtp, data);
+    if (!isValid) {
+      res.status(400).json({ message: "Invalid OTP" });
     }
 
-    async verifyOtp(req, res){
-        // logic
-        const { otp, hash, phone } = req.body;
-        if(!otp || !hash || !phone ){
-            res.status(400).json({message: 'All field are required'});
-        }
-        const [hashedOtp, expires] = hash.split('.');
-        if(Date.now() > +expires) {
-            res.status(400).json({message: 'otp has expired'});
-        }
-        const data = `${phone}.${otp}.${expires}`;
+    let user;
 
-        const isValid = otpService.verifyOtp(hashedOtp, data);
-        if(!isValid){
-            res.status(400).json({ message:'Invalid OTP'});
-        }
-
-        let user;
-        let accessToken;
-        let refressToken;
-
-        try{
-            user = await userService.findUser({ phone });
-            if(!user){
-               user = await userService.createUser({ phone });
-            }
-        }catch(err) {
-            console.log(err);
-            res.status(500).json({ message: 'DB error' });
-        }
-
-        //Token
-        tokenService.generateTokens()
+    try {
+      user = await userService.findUser({ phone });
+      if (!user) {
+        user = await userService.createUser({ phone });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "DB error" });
     }
+
+    //Token
+    const { accessToken, refreshToken } = tokenService.generateTokens({
+      _id: user._id,
+      activated: false,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+    });
+
+    res.json({ accessToken });
+  }
 }
 
 module.exports = new AuthController();
--- backend/services/token-service.js
+++ backend/services/token-service.js
@@ -1,8 +1,17 @@
 const jwt = require('jsonwebtoken');
-
+const accessTokenSecret = process.env.JWT_ACCESS_TOKEN_SECRET;
+const refressTokenSecret = process.env.JWT_REFRESH_TOKEN_SECRET;
 class TokenService {
-    generateTokens() {
-        
+    generateTokens(payload) {
+        const accessToken = jwt.sign(payload, accessTokenSecret, {
+            expiresIn: '1h'
+        });
+
+        const refreshToken = jwt.sign(payload, refressTokenSecret, {
+            expiresIn: '1y'
+        });
+
+        return { accessToken, refreshToken };
     }
 
     
